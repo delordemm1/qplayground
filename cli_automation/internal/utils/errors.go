df0a62415f690af609d424dbfb79311e3c319dfc
Emmanuel Ogbonna <46292455+delordemm1@users.noreply.github.com>
2025-08-04 12:36:52 +0100

    remove buggy cli

--- cli_automation/internal/utils/errors.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import "errors"
-
-var (
-	ErrNotFound       = errors.New("not found")
-	ErrConflict       = errors.New("resource already exists")
-	ErrInvalidRequest = errors.New("invalid request")
-)
